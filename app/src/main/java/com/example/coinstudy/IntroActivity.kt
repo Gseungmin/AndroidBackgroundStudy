3b762040730fe61e68f3c2cdb83954d460f8c0bd
Ji Seungmin <cswcsm02@gmail.com>
2022-12-14 17:29:33 +0900

    FEAT: Spalsh 화면 기능 구현 완료

---
+++ app/src/main/java/com/example/coinstudy/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.example.coinstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.coinstudy.databinding.ActivityIntroBinding
+import com.example.coinstudy.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        Timber.d("onCreate")
+    }
+}
