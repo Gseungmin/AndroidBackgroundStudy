b33474c1083cb66b9b3650c72e01ac8901a0c51a
Ji Seungmin <cswcsm02@gmail.com>
2022-12-14 18:46:20 +0900

    RecyclerView 완성 및 재활용 문제 해결

--- app/src/main/java/com/example/coinstudy/view/adapter/SelectRVAdapter.kt
+++ app/src/main/java/com/example/coinstudy/view/adapter/SelectRVAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.coinstudy.view.adapter
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coinstudy.R
 import com.example.coinstudy.dataModel.CurrentPriceResult
@@ -11,9 +14,12 @@ import com.example.coinstudy.dataModel.CurrentPriceResult
 class SelectRVAdapter(val context: Context, val coinPriceList : List<CurrentPriceResult>)
     : RecyclerView.Adapter<SelectRVAdapter.ViewHolder>() {
 
+    val selectedCoinList = ArrayList<String>()
 
     inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
-
+        val coinName : TextView = view.findViewById(R.id.coinName)
+        val coinPriceUpDown : TextView = view.findViewById(R.id.coinPriceUpDown)
+        val likeImage : ImageView = view.findViewById(R.id.likeBtn)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,6 +29,41 @@ class SelectRVAdapter(val context: Context, val coinPriceList : List<CurrentPric
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        holder.coinName.text = coinPriceList[position].coinName
+
+        val fluctate_24H = coinPriceList[position].coinInfo.fluctate_24H
+
+        if (fluctate_24H.contains("-")) {
+            holder.coinPriceUpDown.text = "하락입니다"
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#114fed"))
+        } else {
+            holder.coinPriceUpDown.text = "상승입니다"
+            holder.coinPriceUpDown.setTextColor(Color.parseColor("#ed2e11"))
+        }
+
+        val likeImage = holder.likeImage
+        val currentCoin = coinPriceList[position].coinName
+
+        /**
+         * recyclerView 재활용 문제 해결
+         * */
+        if (selectedCoinList.contains(currentCoin)) {
+            likeImage.setImageResource(R.drawable.like_red)
+        } else {
+            likeImage.setImageResource(R.drawable.like_grey)
+        }
+
+        likeImage.setOnClickListener{
+
+            if (selectedCoinList.contains(currentCoin)) {
+                likeImage.setImageResource(R.drawable.like_grey)
+                selectedCoinList.remove(currentCoin)
+            } else {
+                likeImage.setImageResource(R.drawable.like_red)
+                selectedCoinList.add(currentCoin)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
