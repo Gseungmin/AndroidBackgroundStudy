c12434c263390d23c0d4cefed9d2b81501f4881b
Ji Seungmin <cswcsm02@gmail.com>
2022-12-14 18:32:31 +0900

    코인 선택 Adapter 셋팅

--- app/src/main/java/com/example/coinstudy/view/SelectActivity.kt
+++ app/src/main/java/com/example/coinstudy/view/SelectActivity.kt
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.coinstudy.R
 import com.example.coinstudy.databinding.ActivityIntroBinding
 import com.example.coinstudy.databinding.ActivitySelectBinding
+import com.example.coinstudy.view.adapter.SelectRVAdapter
 import timber.log.Timber
 
 class SelectActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySelectBinding
     private val viewModel : SelectViewModel by viewModels()
+
+    private lateinit var selectRVAdapter: SelectRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -25,6 +29,12 @@ class SelectActivity : AppCompatActivity() {
 
         viewModel.getCurrentCoinList()
         viewModel.currentPriceResult.observe(this, {
+
+            selectRVAdapter = SelectRVAdapter(this, it)
+
+            binding.coinListRV.adapter = selectRVAdapter
+            binding.coinListRV.layoutManager = LinearLayoutManager(this)
+
             Timber.d(it.toString())
         })
     }
---
+++ app/src/main/java/com/example/coinstudy/view/adapter/SelectRVAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.coinstudy.view.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coinstudy.R
+import com.example.coinstudy.dataModel.CurrentPriceResult
+
+class SelectRVAdapter(val context: Context, val coinPriceList : List<CurrentPriceResult>)
+    : RecyclerView.Adapter<SelectRVAdapter.ViewHolder>() {
+
+
+    inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.intro_coin_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return coinPriceList.size
+    }
+
+
+}
