b8d1f1d925b49006532e980a614bdfdbf38db482
Ji Seungmin <cswcsm02@gmail.com>
2022-12-14 17:42:53 +0900

    인트로 화면 구현 완료

--- app/src/main/java/com/example/coinstudy/view/IntroFragment1.kt
+++ app/src/main/java/com/example/coinstudy/view/IntroFragment1.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.NavInflater
+import androidx.navigation.Navigation
 import com.example.coinstudy.R
 import com.example.coinstudy.databinding.FragmentIntro1Binding
 
@@ -21,9 +24,16 @@ class IntroFragment1 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
         _binding = FragmentIntro1Binding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.nextBtn.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_introFragment1_to_introFragment2)
+        }
     }
 
     //viewBinding이 더이상 필요 없을 경우 null 처리 필요
