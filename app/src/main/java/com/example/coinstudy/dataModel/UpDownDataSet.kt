a25780cab39f93be0c63d314bbdcc0a7315b823f
Ji Seungmin <cswcsm02@gmail.com>
2022-12-14 23:29:04 +0900

    WorkManager 기능 완성

---
+++ app/src/main/java/com/example/coinstudy/dataModel/UpDownDataSet.kt
@@ -0,0 +1,7 @@
+package com.example.coinstudy.dataModel
+
+data class UpDownDataSet (
+
+    val coinName : String,
+    val upDownPrice : String
+        )
--- app/src/main/java/com/example/coinstudy/view/main/MainViewModel.kt
+++ app/src/main/java/com/example/coinstudy/view/main/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.example.coinstudy.view.main
 import androidx.lifecycle.*
 import com.example.coinstudy.dataModel.CurrentPrice
 import com.example.coinstudy.dataModel.CurrentPriceResult
+import com.example.coinstudy.dataModel.UpDownDataSet
 import com.example.coinstudy.dataStore.MyDataStore
 import com.example.coinstudy.network.model.CurrentPriceList
 import com.example.coinstudy.network.repository.DBRepository
@@ -12,6 +13,7 @@ import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import timber.log.Timber
 
 class MainViewModel : ViewModel() {
@@ -20,10 +22,17 @@ class MainViewModel : ViewModel() {
 
     lateinit var selectedCoinList : LiveData<List<InterestCoinEntity>>
 
+    private val _arr15min = MutableLiveData<List<UpDownDataSet>>()
+    val arr15min : LiveData<List<UpDownDataSet>>
+        get() = _arr15min
+    private val _arr30min = MutableLiveData<List<UpDownDataSet>>()
+    val arr30min : LiveData<List<UpDownDataSet>>
+        get() = _arr30min
+    private val _arr45min = MutableLiveData<List<UpDownDataSet>>()
+    val arr45min : LiveData<List<UpDownDataSet>>
+        get() = _arr45min
+
     //CoinListFragment
-
-    //PriceChangeFragment
-
     fun getAllInterestCoinData() = viewModelScope.launch {
 
         val coinList = dbRepository.getAllInterestCoinData().asLiveData()
@@ -39,4 +48,55 @@ class MainViewModel : ViewModel() {
             }
             dbRepository.updateInterestCoinData(interestCoinEntity)
         }
+
+
+    //PriceChangeFragment
+    fun getAllSelectedCoinData() = viewModelScope.launch(Dispatchers.IO) {
+
+        //1. 관심있다고 선택한 코인 리스트 가져오기
+        val selectedCoinData = dbRepository.getAllInterestSelectedCoinData()
+
+        val arr15min = ArrayList<UpDownDataSet>()
+        val arr30min = ArrayList<UpDownDataSet>()
+        val arr45min = ArrayList<UpDownDataSet>()
+
+        //2. 코인 하나씩 가져옴
+        for (data in selectedCoinData) {
+
+            val coinName = data.coin_name
+            //3. 저장된 코인 가격 리스트 가져옴
+            val oneCoinData = dbRepository.getOneSelectedCoinData(coinName)
+
+
+            //4. 시간대마다 어떻게 변경되었는지 알려주는 로직 작성
+            val size = oneCoinData.size
+
+            //현재와 15분 전을 비교하려면 데이터가 2개는 필요
+            if (size > 1) {
+                val changedPrice = oneCoinData[0].price.toDouble() - oneCoinData[1].price.toDouble()
+                val upDownDataSet = UpDownDataSet(coinName, changedPrice.toString())
+                arr15min.add(upDownDataSet)
+            }
+
+            //현재와 30분 전을 비교하려면 데이터가 2개는 필요
+            if (size > 2) {
+                val changedPrice = oneCoinData[0].price.toDouble() - oneCoinData[2].price.toDouble()
+                val upDownDataSet = UpDownDataSet(coinName, changedPrice.toString())
+                arr30min.add(upDownDataSet)
+            }
+
+            //현재와 45분 전을 비교하려면 데이터가 2개는 필요
+            if (size > 3) {
+                val changedPrice = oneCoinData[0].price.toDouble() - oneCoinData[3].price.toDouble()
+                val upDownDataSet = UpDownDataSet(coinName, changedPrice.toString())
+                arr45min.add(upDownDataSet)
+            }
+        }
+
+        withContext(Dispatchers.Main) {
+            _arr15min.value = arr15min
+            _arr30min.value = arr30min
+            _arr45min.value = arr45min
+        }
+    }
 }
